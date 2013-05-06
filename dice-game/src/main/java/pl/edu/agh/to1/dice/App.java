65abab474dab0b0cdedc24d08331d239ec31d889
s1mplex <kacperoza@gmail.com>
2013-05-06 22:28:45 +0200

    Changed comment in App

--- dice-game/src/main/java/pl/edu/agh/to1/dice/App.java
+++ dice-game/src/main/java/pl/edu/agh/to1/dice/App.java
@@ -8,13 +8,7 @@ import pl.edu.agh.to1.dice.logic.GameConfigurator;
 public class App {
     public static void main(String[] args) {
    	
-    	// TODO - kod ponizej wrzucic w GameStarter:
-    	//			GameStarter (korzystajac z jakiegos IO handlera, jezeli chcemy miec kontakt ze swiatem zewnetrznym w jednym miejscu) 
-    	//			zapyta o ilosc graczy, imiona, typ rozgrywki
-    	//			stworzy graczy, tablice wynikow, stworzy gre i zwroci ja tutaj.
-    	//			mysle ze od tego trzeba zaczac, i ze serializacje tez by wypadalo jakos tutaj zaczepic (skoro bedziemy miec tu tablice wynikow,
-    	//			mozna jakos z niej wyciagnac punkty kazdego gracza i je zachowac)
-    	// TODO: Historia gier, tworzenie tablicy wynikow?, bot
+    	// TODO: Wypadaloby tworzyc tablice wynikow gdzie indziej
     	
     	GameConfigurator configurator = new ConsoleGameConfigurator();
         DiceGame game = configurator.loadConfiguration();
